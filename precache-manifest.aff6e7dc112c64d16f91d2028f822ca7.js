e6ad5d8879140aad049484ea057433bc9034c083
KsuParkhamchuk <ksusha.rr@yandex.ru>
2020-06-22 16:09:58 +0300

    Updates

---
+++ precache-manifest.aff6e7dc112c64d16f91d2028f822ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0dee0f0bda045e04c1529695be56acb",
+    "url": "/nasa-img-gallery/index.html"
+  },
+  {
+    "revision": "c91bcd8afff780d02608",
+    "url": "/nasa-img-gallery/static/css/main.1e429819.chunk.css"
+  },
+  {
+    "revision": "0a9ef25ed159ac28b9c7",
+    "url": "/nasa-img-gallery/static/js/2.71e7365b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nasa-img-gallery/static/js/2.71e7365b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91bcd8afff780d02608",
+    "url": "/nasa-img-gallery/static/js/main.b8675c58.chunk.js"
+  },
+  {
+    "revision": "2e0e83b4503708aa4f72",
+    "url": "/nasa-img-gallery/static/js/runtime-main.1c1a74d5.js"
+  }
+]);
--- src/history/index.tsx
+++
@@ -1,8 +0,0 @@
-import createHistory from 'history/createHashHistory'
-
-const history = createHistory({
-  basename: '', // portal#/home
-  hashType: 'slash' // is #/home
-})
-
-export default history
